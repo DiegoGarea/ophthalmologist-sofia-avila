e416874d4a60a1906c22f944a2e154cf252ba417
Diego G <d.g.g.87@hotmail.com>
2023-09-25 17:50:07 -0600

    added sofia info

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,26 @@
 import logo from './logo.jpg';
 import person from './person.png';
-import service1 from './service1.png'
-import service2 from './service2.png'
-import service3 from './service3.png'
+
+import service1 from './service1.png';
+import service2 from './service2.png';
+import service3 from './service3.png';
+
+import whatsapp from './whatsapp.png';
+import instagram from './instagram.png';
+import facebook from './facebook.png';
 
 import hamburguer from './hamburger.svg';
 import close from './close.svg';
 
-export {logo, person, hamburguer, close, service1, service2, service3};
+export {
+  logo,
+  person,
+  hamburguer,
+  close,
+  service1,
+  service2,
+  service3,
+  facebook,
+  instagram,
+  whatsapp,
+};
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,43 +1,63 @@
 import {
-   service1, 
-   service2, 
-   service3
-   } from "../assets";
+  service1,
+  service2,
+  service3,
+  whatsapp,
+  instagram,
+  facebook,
+} from '../assets';
 
 const navLinks = [
   {
-    id: "about",
-    title: "Conoceme",
+    id: 'about',
+    title: 'Conoceme',
   },
   {
-    id: "services",
-    title: "Servicios",
+    id: 'services',
+    title: 'Servicios',
   },
   {
-    id: "Contact",
-    title: "Contactame",
+    id: 'Contact',
+    title: 'Contactame',
   },
 ];
 
 const services = [
   {
     image: service1,
-    title: "Service 1",
+    title: 'Service 1',
     description:
-      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam nulla, harum sunt blanditiis incidunt eos perspiciatis sit eum  adipisci nostrum sed ab vitae facilis rerum, aperiam odio enim reprehenderit!",
+      ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam nulla, harum sunt blanditiis incidunt eos perspiciatis sit eum  adipisci nostrum sed ab vitae facilis rerum, aperiam odio enim reprehenderit!',
   },
   {
     image: service2,
-    title: "Service 2",
+    title: 'Service 2',
     description:
-      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam nulla, harum sunt blanditiis incidunt eos perspiciatis sit eum  adipisci nostrum sed ab vitae facilis rerum, aperiam odio enim reprehenderit!",
+      ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam nulla, harum sunt blanditiis incidunt eos perspiciatis sit eum  adipisci nostrum sed ab vitae facilis rerum, aperiam odio enim reprehenderit!',
   },
   {
     image: service3,
-    title: "Service 3",
+    title: 'Service 3',
     description:
-      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam nulla, harum sunt blanditiis incidunt eos perspiciatis sit eum  adipisci nostrum sed ab vitae facilis rerum, aperiam odio enim reprehenderit!",
+      ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe ullam nulla, harum sunt blanditiis incidunt eos perspiciatis sit eum  adipisci nostrum sed ab vitae facilis rerum, aperiam odio enim reprehenderit!',
   },
 ];
 
-export { navLinks, services };
+const socials = [
+  {
+    image: whatsapp,
+    link: 'https://wa.me/+523314662319',
+  },
+  {
+    image: instagram,
+    link: 'https://www.instagram.com/dra.dianasofiaavila/',
+  },
+  {
+    image: facebook,
+    link: 'https://www.facebook.com/profile.php?id=100095421096881',
+  },
+];
+
+// este es el correo de sofia para agregar al footer dsofiaavilam@gmail.com
+
+export {navLinks, services, socials};
